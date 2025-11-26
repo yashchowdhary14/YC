33cc1a42e3b6585d616bc12552c5eb3b7f8cb309
Yash Chowdhary <yashchowdhary778@gmail.com>
2025-11-26 10:49:09 +0000

    change the web preview to phone preview, can you do that

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,5 +1,23 @@
-import * as React from "react"
 
+'use client';
+
+import { useEffect, useState } from "react"
+
+// This hook is used to determine if the app should render the mobile UI.
+// For development purposes, you can force it to return true.
 export function useIsMobile() {
-  return true
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIsMobile = () => {
+      // You can adjust this breakpoint as needed
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkIsMobile();
+    window.addEventListener('resize', checkIsMobile);
+    return () => window.removeEventListener('resize', checkIsMobile);
+  }, []);
+
+  return true; // Set to true to force phone preview
 }
