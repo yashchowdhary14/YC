f9815020570cfd691386892d01f0dc6e85b2bbd4
Yash Chowdhary <yashchowdhary778@gmail.com>
2025-11-26 11:18:29 +0000

    do the same in phone ui for status bar, it is still black

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <meta name="theme-color" content="#000000" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
